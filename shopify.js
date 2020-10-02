025801da2630ab52b598d653ad5f4091ea8cd7e4
Brian Cornielle <6381083-bcorniellepl@users.noreply.gitlab.com>
2020-10-02 10:30:09 -0400

    guardando todo y los resultados

--- shopify.js
+++ shopify.js
@@ -14,96 +14,67 @@ let queryCreateBulk = `mutation {
             node {
               id
               createdAt
-              updatedAt
-                customer{
-                  firstName
-                  lastName
+              totalPrice
+              name
+              customer{
+                firstName
+                lastName
+              }
+              lineItems{
+                edges{
+                  node{
+                    sku,
+                    quantity
+                  }
                 }
+              }
+              transactions{
+                status
+                gateway
+                authorizationCode
               }
             }
           }
         }
-      """
-    ) {
-      bulkOperation {
-        id
-        status
       }
-      userErrors {
-        field
-        message
-      }
+    """
+  ) {
+    bulkOperation {
+      id
+      status
     }
-  }`
-
-  let queryDownloadBulk = `{
-    node(id: "gid://shopify/BulkOperation/179735527490") {
-      ... on BulkOperation {
-        id
-        status
-        errorCode
-        createdAt
-        completedAt
-        objectCount
-        fileSize
-        url
-        partialDataUrl
-      }
+    userErrors {
+      field
+      message
     }
-  }`
-  // return `{
-  //   ${dataArray.length>0 && parseInt(availableItems)>1?
-  //   `orders(first:${availableItems},after:"${cursor}") {`:
-  //   `orders(first:250,query:"created_at:>2020-03-01"){`}
-  //       pageInfo { hasNextPage }
-  //       edges {
-  //         cursor
-  //         node {
-  //           id,
-  //           createdAt
-  //           customer{
-  //               firstName
-  //               lastName
-  //           }
-  //         }
-  //       }
-  //     }
-  // }`
+  }
+}`
+let queryDownloadBulk = `{
+  node(id: "gid://shopify/BulkOperation/180570161218") {
+    ... on BulkOperation {
+      id
+      status
+      errorCode
+      createdAt
+      completedAt
+      objectCount
+      fileSize
+      url
+      partialDataUrl
+    }
+  }
+}`
 
-function autoFetch(url, request){
-  // let dataArray = [];
-  //   setInterval(() =>{
-  //     let query = generateQuery(
-  //       dataArray,
-  //       cursor
-  //     ) 
+function autoFetch(url, request, mode ){
       request.body = queryDownloadBulk
       fetch(url, request)
       .then(res => res.json())
       .then(result => {
-        if(result.errors){
-          console.log(result.errors)
+        if(result.data.bulkOperationRunQuery!== undefined){
+          console.log(result.data.bulkOperationRunQuery)
           return
         }
         console.log(result)
-        // availableItems = result.extensions.cost.throttleStatus.restoreRate
-        // console.log(availableItems)
-        // cursor =  result.data.orders.edges[249].cursor
-        // console.log(cursor)
-        //   return
-        //   cursor =  result.data.orders.edges[249].cursor
-        //   console.log(cursor, 'los cursores?')
-        //   dataArray.push(result.data.orders.edges)
-        //   console.log(dataArray, 'checking');
-        // //   console.log(cursor)
-        // //   console.log(order)
-        // //   // orderArray.push(`'${order.id} ${order.created_at}'${'\n'}`)
-        // //   // fs.writeFile("./ids.json", orderArray, function(err) {
-        // //   //     if(err){
-        // //   //         console.log(err);
-        // //   //     }
-        // //   // })
-        // // })
       }).catch(err=>{
         console.log(err)
       })
